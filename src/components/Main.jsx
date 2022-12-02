2c062c87d8a99f592a3a72f80324966aaa5e4d6d
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-12-02 21:17:16 +0100

    Exercise 10.5: polished reviewed repositories list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,14 +7,26 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: "#e1e4e8",
   },
+  // reposContainer: {
+  // lignItems: "center",
+  // justifyContent: "center",
+  // padding: 16,
+  // baddingBottom: 16,
+  // backgroundColor: "white",
+  // marginBottom: 16,
+  // flex: 1,
+  // },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
+      {/* <View style={styles.reposContainer}> */}
       <RepositoryList />
+      {/* </View> */}
     </View>
   );
 };
